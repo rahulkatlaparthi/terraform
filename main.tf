fb3c536179e3f178a37c5adeccbd538d23fbd13e
Uday kumar <udayjothula@gmail.com>
2024-05-10 03:21:57 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
+// resource "azurerm_resource_group" "example" {
+//   name     = "example-resources"
+//   location = "West Europe"
+// }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"

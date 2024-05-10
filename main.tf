11923b28fb38f93f4dc65904363da8f5f21a57a3
Uday kumar <udayjothula@gmail.com>
2024-05-10 05:10:57 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -70,6 +70,7 @@ provider "azurerm" {
 resource "azurerm_user_assigned_identity" "example" {
   name="example-identity"
   resource_group_name       = var.resource_group_name
+  location                  = var.location
   
 }
 

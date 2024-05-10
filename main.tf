76e07ae1e4d6e2ed149cbbe1f06846e6c73c8993
Uday kumar <udayjothula@gmail.com>
2024-05-10 03:20:41 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "service" {
+  name                 = "service"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  enforce_private_link_service_network_policies = true
+}

f323a705cb41f9a5ba142ab62ea90ff15f5ece34
Uday kumar <udayjothula@gmail.com>
2024-05-10 10:54:50 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "dev"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "samplest56h"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+  // backend "azurerm" {
+  //   resource_group_name  = "dev"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+  //   storage_account_name = "samplest56h"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  //   container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  //   key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  // }
 }
 
 # Configure the Microsoft Azure Provider
@@ -205,7 +205,7 @@ provider "azurerm" {
 // }
 
 resource "azurerm_application_insights" "example" {
-  name                = "tf-test-appinsights"
+  name                = "tf-dev-appinsights"
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"

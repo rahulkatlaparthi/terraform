44208fef8dcc86a9e78f35b369aea50e2c5fd5c5
Uday kumar <udayjothula@gmail.com>
2024-05-10 02:59:16 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "azurerm" {
           #version = "=2.20.0"  #version = "=0.13.1" 
            version = "=2.20.0"
 		   version= "2.4.0"
-           subscription_id = "2bb92624-217a-45f4-a6c0-9da57926d5bd" 
-            tenant_id       = "01baeab8-27f4-42c6-b537-4d968a11298c"  
-            client_id       = "914fdd58-40ba-470d-b959-586d7e517592"  
+           subscription_id = "26c0f402-6550-45b8-a992-cc6da4656d81" 
+            tenant_id       = "26c0f402-6550-45b8-a992-cc6da4656d81"  
+            client_id       = "12820d3c-c69e-400a-a835-e194068d8363"  
             features {}  
 }
 resource "azurerm_resource_group" "RS1" {

da0188a7ec39e4b06fec451e2ba63f9d651e1e71
Uday kumar <udayjothula@gmail.com>
2024-05-10 04:30:43 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "dev"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "samplest56h"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 # Configure the Microsoft Azure Provider
@@ -47,16 +53,16 @@ provider "azurerm" {
 //   }
 // }
 
-resource "azurerm_private_endpoint" "example" {
-  name                = "ple-sql"
-  location            = "West Europe"
-  resource_group_name = "example-resources"
-  subnet_id           = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Network/virtualNetworks/example-network/subnets/service"
+// resource "azurerm_private_endpoint" "example" {
+//   name                = "ple-sql"
+//   location            = "West Europe"
+//   resource_group_name = "example-resources"
+//   subnet_id           = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Network/virtualNetworks/example-network/subnets/service"
 
-  private_service_connection {
-    name                           = "sqlserverple"
-    private_connection_resource_id = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Sql/servers/mssqlserverrahul73"
-    is_manual_connection           = false
-    subresource_names=["sqlServer"]
-  }
-}
+//   private_service_connection {
+//     name                           = "sqlserverple"
+//     private_connection_resource_id = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Sql/servers/mssqlserverrahul73"
+//     is_manual_connection           = false
+//     subresource_names=["sqlServer"]
+//   }
+// }

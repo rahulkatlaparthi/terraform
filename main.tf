bfb49f20f07e16f1752a51fed767051a64f34628
Uday kumar <udayjothula@gmail.com>
2024-05-10 03:36:50 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -34,3 +34,15 @@ resource "azurerm_subnet" "service" {
 
   enforce_private_link_service_network_policies = true
 }
+resource "azurerm_sql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = "example-resources"
+  location                     = "West Europe""
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  tags = {
+    environment = "production"
+  }
+}

3c9e742fe8ac2017f8090b5da407bcf050d28d6b
Uday kumar <udayjothula@gmail.com>
2024-05-10 06:02:15 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -73,23 +73,23 @@ provider "azurerm" {
   
 // }
 
-// resource "azurerm_storage_account" "storeacc" {
-//   name                      = var.storage_account_name
-//   resource_group_name       = var.resource_group_name
-//   location                  = var.location
-//   access_tier= var.access_tier
-//   account_kind              = var.account_kind
-//   account_tier              = var.account_tier
-//   account_replication_type  = var.account_replication_type
-//   enable_https_traffic_only = true
-//   min_tls_version           = var.min_tls_version
-//   tags                      = var.tags
-//   identity{
-//     type="UserAssigned"
-//     identity_ids=[azurerm_user_assigned_identity.example.id]
-//   }
-//   depends_on=[azurerm_user_assigned_identity.example]
-// }
+resource "azurerm_storage_account" "storeacc" {
+  name                      = var.storage_account_name
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
+  access_tier= var.access_tier
+  account_kind              = var.account_kind
+  account_tier              = var.account_tier
+  account_replication_type  = var.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = var.min_tls_version
+  tags                      = var.tags
+  identity{
+    type="UserAssigned"
+    identity_ids=[azurerm_user_assigned_identity.example.id]
+  }
+  depends_on=[azurerm_user_assigned_identity.example]
+}
 
 
 resource "azurerm_private_endpoint" "storageprivate" {
@@ -104,4 +104,5 @@ resource "azurerm_private_endpoint" "storageprivate" {
     is_manual_connection           = false
     subresource_names=["blob"]
   }
+   depends_on=[azurerm_storage_account.storeacc]
 }

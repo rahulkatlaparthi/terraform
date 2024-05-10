0149fb769dbc20ce416342a55eaf8f7c3f22cbe9
Uday kumar <udayjothula@gmail.com>
2024-05-10 05:14:33 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "azurerm_storage_account" "storeacc" {
   resource_group_name       = var.resource_group_name
   location                  = var.location
   account_kind              = var.account_kind
-  account_tier              = var.account_tier
+  account_tier              = var.access_tier
   account_replication_type  = var.skuname
   enable_https_traffic_only = true
   min_tls_version           = var.min_tls_version

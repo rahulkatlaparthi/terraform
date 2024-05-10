325c99b363b1d0ea1a8b4f7e962414fc5cbf3612
Uday kumar <udayjothula@gmail.com>
2024-05-10 03:12:25 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {  
           #version = "=2.20.0"  #version = "=0.13.1" 
            version = "=2.20.0"
-		   version= "2.4.0"
+		   
            subscription_id = "26c0f402-6550-45b8-a992-cc6da4656d81" 
             tenant_id       = "26c0f402-6550-45b8-a992-cc6da4656d81"  
             client_id       = "12820d3c-c69e-400a-a835-e194068d8363"  

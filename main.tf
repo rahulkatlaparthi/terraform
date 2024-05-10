ca75da1bb4e428dd6e37b76e1440eec8c90cef32
Uday kumar <udayjothula@gmail.com>
2024-05-10 04:37:24 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -66,3 +66,25 @@ provider "azurerm" {
 //     subresource_names=["sqlServer"]
 //   }
 // }
+
+resource "azurerm_storage_account" "storeacc" {
+  name                      = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24)
+  resource_group_name       = local.resource_group_name
+  location                  = local.location
+  account_kind              = var.account_kind
+  account_tier              = local.account_tier
+  account_replication_type  = local.account_replication_type
+  enable_https_traffic_only = true
+  min_tls_version           = var.min_tls_version
+  tags                      = merge({ "ResourceName" = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24) }, var.tags, )
+
+  dynamic "identity" {
+    for_each = var.managed_identity_type != null ? [1] : []
+    content {
+      type         = var.managed_identity_type
+      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
+    }
+  }
+
+
+}

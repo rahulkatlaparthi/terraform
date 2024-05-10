8ca971847535442f1eb2cf7c9e5cb62e192a6996
Uday kumar <udayjothula@gmail.com>
2024-05-10 06:35:38 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -66,43 +66,55 @@ provider "azurerm" {
 //     subresource_names=["sqlServer"]
 //   }
 
-resource "azurerm_user_assigned_identity" "example" {
-  name="example-identity"
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
+// resource "azurerm_user_assigned_identity" "example" {
+//   name="example-identity"
+//   resource_group_name       = var.resource_group_name
+//   location                  = var.location
   
-}
+// }
 
-resource "azurerm_storage_account" "storeacc" {
-  name                      = var.storage_account_name
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  access_tier= var.access_tier
-  account_kind              = var.account_kind
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  enable_https_traffic_only = true
-  min_tls_version           = var.min_tls_version
-  tags                      = var.tags
-  identity{
-    type="UserAssigned"
-    identity_ids=[azurerm_user_assigned_identity.example.id]
+// resource "azurerm_storage_account" "storeacc" {
+//   name                      = var.storage_account_name
+//   resource_group_name       = var.resource_group_name
+//   location                  = var.location
+//   access_tier= var.access_tier
+//   account_kind              = var.account_kind
+//   account_tier              = var.account_tier
+//   account_replication_type  = var.account_replication_type
+//   enable_https_traffic_only = true
+//   min_tls_version           = var.min_tls_version
+//   tags                      = var.tags
+//   identity{
+//     type="UserAssigned"
+//     identity_ids=[azurerm_user_assigned_identity.example.id]
+//   }
+//   depends_on=[azurerm_user_assigned_identity.example]
+// }
+
+
+// resource "azurerm_private_endpoint" "storageprivate" {
+//   name                = "ple-storage"
+//   location            = "West Europe"
+//   resource_group_name = "example-resources"
+//   subnet_id           = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Network/virtualNetworks/example-network/subnets/service"
+
+//   private_service_connection {
+//     name                           = "storageple"
+//     private_connection_resource_id = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Storage/storageAccounts/rahulstg619"
+//     is_manual_connection           = false
+//     subresource_names=["blob"]
+//   }
+//    depends_on=[azurerm_storage_account.storeacc]
+// }
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kind                = var.app_service_plan_kind
+  reserved            = true
+
+  sku {
+    tier = var.app_service_plan_tier
+    size = var.app_service_plan_size
   }
-  depends_on=[azurerm_user_assigned_identity.example]
-}
-
-
-resource "azurerm_private_endpoint" "storageprivate" {
-  name                = "ple-storage"
-  location            = "West Europe"
-  resource_group_name = "example-resources"
-  subnet_id           = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Network/virtualNetworks/example-network/subnets/service"
-
-  private_service_connection {
-    name                           = "storageple"
-    private_connection_resource_id = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Storage/storageAccounts/rahulstg619"
-    is_manual_connection           = false
-    subresource_names=["blob"]
-  }
-   depends_on=[azurerm_storage_account.storeacc]
 }

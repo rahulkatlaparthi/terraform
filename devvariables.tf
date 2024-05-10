a39baeb492820954ea7e09891880e481014e7fd9
Uday kumar <udayjothula@gmail.com>
2024-05-10 10:54:22 +0000

    Updated devvariables.tf

--- devvariables.tf
+++ devvariables.tf
@@ -1,1 +1,16 @@
-h
+  description = "Whether to create resource group and use it for all networking resources"
+  default     = false
+  type        = bool
+}
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+  default     = "dev"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default     = "East US"
+  type        = string
+}

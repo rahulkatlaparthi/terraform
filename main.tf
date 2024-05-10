a7310922fe22650b7fb85349e7ae912e186c3354
Uday kumar <udayjothula@gmail.com>
2024-05-10 04:08:07 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -34,15 +34,29 @@ provider "azurerm" {
 
 //   enforce_private_link_service_network_policies = true
 // }
-resource "azurerm_sql_server" "example" {
-  name                         = "mssqlserverrahul73"
-  resource_group_name          = "example-resources"
-  location                     = "Central India"
-  version                      = "12.0"
-  administrator_login          = "mradministrator"
-  administrator_login_password = "thisIsDog11"
+// resource "azurerm_sql_server" "example" {
+//   name                         = "mssqlserverrahul73"
+//   resource_group_name          = "example-resources"
+//   location                     = "Central India"
+//   version                      = "12.0"
+//   administrator_login          = "mradministrator"
+//   administrator_login_password = "thisIsDog11"
 
-  tags = {
-    environment = "production"
+//   tags = {
+//     environment = "production"
+//   }
+// }
+
+resource "azurerm_private_endpoint" "example" {
+  name                = "ple-sql"
+  location            = "Central India"
+  resource_group_name = "example-resources"
+  subnet_id           = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Network/virtualNetworks/example-network/subnets/service"
+
+  private_service_connection {
+    name                           = "sqlserverple"
+    private_connection_resource_id = "/subscriptions/aa8cf85a-4850-4a20-a36c-298efad14547/resourceGroups/example-resources/providers/Microsoft.Sql/servers/mssqlserverrahul73"
+    is_manual_connection           = false
+    subresource_names=["sqlServer"]
   }
 }

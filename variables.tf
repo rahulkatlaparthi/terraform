387c5d6c4f3c5745e7a24e527dc9089df6fe9a68
Uday kumar <udayjothula@gmail.com>
2024-05-10 11:15:27 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "create_resource_group" {
-  description = "Whether to create resource group and use it for all networking resources"
-  default     = false
-  type        = bool
-}
+// variable "create_resource_group" {
+//   description = "Whether to create resource group and use it for all networking resources"
+//   default     = false
+//   type        = bool
+// }
 
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
@@ -18,124 +18,124 @@ variable "location" {
 
 
 
-variable "storage_account_name" {
-  description = "The name of the azure storage account"
-  default     = "rahulstg619"
-  type        = string
-}
+// variable "storage_account_name" {
+//   description = "The name of the azure storage account"
+//   default     = "rahulstg619"
+//   type        = string
+// }
 
 
-variable "sku_name" {
-  description = "The name of the azure storage account"
-  default     = "standard"
-  type        = string
-}
+// variable "sku_name" {
+//   description = "The name of the azure storage account"
+//   default     = "standard"
+//   type        = string
+// }
 
 
-variable "app_service_plan" {
+// variable "app_service_plan" {
   
-  default     = "rahulasp619"
-  type        = string
-}
-variable "app_service_name" {
+//   default     = "rahulasp619"
+//   type        = string
+// }
+// variable "app_service_name" {
   
-  default     = "rahulas619"
-  type        = string
-}
-variable "app_service_plan_kind" {
+//   default     = "rahulas619"
+//   type        = string
+// }
+// variable "app_service_plan_kind" {
   
-  default     = "Linux"
-  type        = string
-}
+//   default     = "Linux"
+//   type        = string
+// }
 
-variable "app_service_plan_tier" {
+// variable "app_service_plan_tier" {
   
-  default     = "Standard"
-  type        = string
-}
+//   default     = "Standard"
+//   type        = string
+// }
 
-variable "app_service_plan_size" {
+// variable "app_service_plan_size" {
   
-  default     = "S1"
-  type        = string
-}
+//   default     = "S1"
+//   type        = string
+// }
 
-variable "account_kind" {
-  description = "The type of storage account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2."
-  default     = "StorageV2"
-  type        = string
-}
+// variable "account_kind" {
+//   description = "The type of storage account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2."
+//   default     = "StorageV2"
+//   type        = string
+// }
 
-variable "key_type" {
+// variable "key_type" {
   
-  default     = "RSA"
-  type        = string
-}
+//   default     = "RSA"
+//   type        = string
+// }
 
-variable "key_size" {
+// variable "key_size" {
   
-  default     = "2048"
-  type        = number
-}
+//   default     = "2048"
+//   type        = number
+// }
 
-variable "key_ops" {
+// variable "key_ops" {
   
-   default     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-  type        = list(string)
-}
+//    default     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+//   type        = list(string)
+// }
 
 
-variable "skuname" {
-  description = "The SKUs supported by Microsoft Azure Storage. Valid options are Premium_LRS, Premium_ZRS, Standard_GRS, Standard_GZRS, Standard_LRS, Standard_RAGRS, Standard_RAGZRS, Standard_ZRS"
-  default     = "Standard_LRS"
-  type        = string
-}
+// variable "skuname" {
+//   description = "The SKUs supported by Microsoft Azure Storage. Valid options are Premium_LRS, Premium_ZRS, Standard_GRS, Standard_GZRS, Standard_LRS, Standard_RAGRS, Standard_RAGZRS, Standard_ZRS"
+//   default     = "Standard_LRS"
+//   type        = string
+// }
 
-variable "access_tier" {
-  description = "Defines the access tier for BlobStorage and StorageV2 accounts. Valid options are Hot and Cool."
-  default     = "Cool"
-  type        = string
-}
+// variable "access_tier" {
+//   description = "Defines the access tier for BlobStorage and StorageV2 accounts. Valid options are Hot and Cool."
+//   default     = "Cool"
+//   type        = string
+// }
 
-variable "account_tier" {
+// variable "account_tier" {
   
-  default     = "Standard"
-  type        = string
-}
+//   default     = "Standard"
+//   type        = string
+// }
 
-variable "account_replication_type" {
+// variable "account_replication_type" {
   
-  default     = "LRS"
-  type        = string
-}
+//   default     = "LRS"
+//   type        = string
+// }
 
-variable "min_tls_version" {
-  description = "The minimum supported TLS version for the storage account"
-  default     = "TLS1_2"
-  type        = string
-}
-
-
+// variable "min_tls_version" {
+//   description = "The minimum supported TLS version for the storage account"
+//   default     = "TLS1_2"
+//   type        = string
+// }
 
 
 
 
 
 
-variable "managed_identity_type" {
-  description = "The type of Managed Identity which should be assigned to the Linux Virtual Machine. Possible values are `SystemAssigned`, `UserAssigned` and `SystemAssigned, UserAssigned`"
-  default     = null
-  type        = string
-}
 
-variable "managed_identity_ids" {
-  description = "A list of User Managed Identity ID's which should be assigned to the Linux Virtual Machine."
-  default     = null
-  type        = list(string)
-}
 
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
+// variable "managed_identity_type" {
+//   description = "The type of Managed Identity which should be assigned to the Linux Virtual Machine. Possible values are `SystemAssigned`, `UserAssigned` and `SystemAssigned, UserAssigned`"
+//   default     = null
+//   type        = string
+// }
+
+// variable "managed_identity_ids" {
+//   description = "A list of User Managed Identity ID's which should be assigned to the Linux Virtual Machine."
+//   default     = null
+//   type        = list(string)
+// }
+
+// variable "tags" {
+//   description = "A map of tags to add to all resources"
+//   type        = map(string)
+//   default     = {}
+// }

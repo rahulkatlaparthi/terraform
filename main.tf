95ce762ac9a66b776f4897b8c2abf4516b06ad39
Uday kumar <udayjothula@gmail.com>
2024-05-10 07:23:34 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -141,3 +141,15 @@ resource "azurerm_app_service" "exampleappservice" {
 
 
 }
+
+resource "azurerm_key_vault" "vault" {
+  name                       = "rahulkykeyvault"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+
+  sku_name                   = var.sku_name
+
+
+
+  
+}

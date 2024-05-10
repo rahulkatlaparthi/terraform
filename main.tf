185ad762351a688e83161bfc78f3dd03a9611115
Uday kumar <udayjothula@gmail.com>
2024-05-10 05:03:00 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -67,24 +67,29 @@ provider "azurerm" {
 //   }
 // }
 
+resoure "azurerm_user_assigned_identity" "example"{
+  name="example-identity"
+  resource_group_name       = var.resource_group_name
+  
+}
+
 resource "azurerm_storage_account" "storeacc" {
   name                      = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24)
-  resource_group_name       = local.resource_group_name
-  location                  = local.location
+  resource_group_name       = var.resource_group_name
+  location                  = var.location
   account_kind              = var.account_kind
-  account_tier              = local.account_tier
-  account_replication_type  = local.account_replication_type
+  account_tier              = var.account_tier
+  account_replication_type  = var.skuname
   enable_https_traffic_only = true
   min_tls_version           = var.min_tls_version
   tags                      = merge({ "ResourceName" = substr(format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result), 0, 24) }, var.tags, )
-
-  dynamic "identity" {
-    for_each = var.managed_identity_type != null ? [1] : []
-    content {
-      type         = var.managed_identity_type
-      identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? var.managed_identity_ids : null
-    }
+  identity{
+    type="UserAssigned"
+    identity_ids=[azurerm_user_assigned_identity.example.id]
   }
+  depends_on=["azurerm_user_assigned_identity.example]
+
+
 
 
 }

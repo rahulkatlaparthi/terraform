2042a3638063879db0c572e40595cac66fbc6ab6
Uday kumar <udayjothula@gmail.com>
2024-05-10 06:55:30 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -126,8 +126,11 @@ resource "azurerm_app_service" "example" {
   app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    java = "1.8"
-    java_container="TOMCAT"
+     linux_fx_version = "TOMCAT|9.0-java11"
+    always_on              = true
+    java_version           = "11"
+    java_container         = "JAVA"
+    java_container_version = "11"
   }
     identity{
     type="UserAssigned"
